abe8ba91a3608e703e91884bc4a83eec4e74648f
theanndyoliver <adm-loren@hotmail.com>
2026-02-25 10:25:40 -0400

    Subtotal e Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -141,6 +141,15 @@ public class Order implements Serializable {
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+	
+	public Double getTotal() {
+		Double sum = 0.0;
+		
+		for(OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
 
 
 
--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -80,6 +80,11 @@ public class OrderItem implements Serializable {
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+	
+	public Double getSubTotal() {
+		return price * quantity;
+		
+	}
 
 
 	@Override
